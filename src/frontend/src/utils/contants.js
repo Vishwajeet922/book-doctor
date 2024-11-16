49fb56f25c5c430aff853102933c5ce46b8336b0
Vishwajeet922 <x23319330@student.ncirl.ie>
2024-11-16 19:11:15 +0000

    Bug fixes for app

--- src/frontend/src/utils/contants.js
+++ src/frontend/src/utils/contants.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080";
+const API_URL = "http://23319330-book-doctor-lb-1389235972.eu-central-1.elb.amazonaws.com";
 const API_VERSION = "/api/v1";
 const API_BASE_URL = `${API_URL}${API_VERSION}`;
 const API_MASTER_DATA_URL = `${API_BASE_URL}/master`;
